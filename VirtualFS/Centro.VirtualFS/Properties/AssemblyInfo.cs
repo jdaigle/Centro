db6c7727bdebb7d92f1d0fb6a0adb4beade03fc2
Joseph Daigle <joseph@cridion.com>
2009-08-19 14:25:04 -0400

    initial commit of VirtualFS

---
+++ VirtualFS/Centro.VirtualFS/Properties/AssemblyInfo.cs
@@ -0,0 +1,31 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Centro VirtualFS API Library")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCompany("Cridion")]
+[assembly: AssemblyProduct("Centro Virtual File System")]
+[assembly: AssemblyCopyright("Copyright © Cridion, LLC 2009")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+[assembly: CLSCompliant(true)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4d143910-ab58-4a6d-a0b1-be63f7106490")]
+
+#if DEBUG
+[assembly: AssemblyVersion("0.1.*")]
+#else
+[assembly: AssemblyVersion("0.1.0.0")]
+#endif
+[assembly: AssemblyFileVersion("0.1")]

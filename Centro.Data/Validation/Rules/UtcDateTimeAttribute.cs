1acae66fbb4cd4bbb406440dd84062d9d85b1a42
Joseph Daigle <joseph@cridion.com>
2009-10-14 00:30:49 -0400

    added some custom validation rules that i'm unlikely to use

---
+++ Centro.Data/Validation/Rules/UtcDateTimeAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using NHibernate.Validator.Engine;
+
+namespace Centro.Data.Validation.Rules
+{
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
+    [ValidatorClass(typeof(UtcDateTimeValidator))]
+    public class UtcDateTimeAttribute : Attribute, IRuleArgs
+    {
+        private string message = "The date 'Kind' must be UTC.";
+        public string Message
+        {
+            get { return message; }
+            set { message = value; }
+        }
+    }
+}
---
+++ Centro.Data/Validation/Rules/UtcDateTimeValidator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Centro.Validation;
+using NHibernate.Validator.Engine;
+
+namespace Centro.Data.Validation.Rules
+{
+    public class UtcDateTimeValidator : NHibernate.Validator.Engine.IValidator
+    {
+        public bool IsValid(object value, IConstraintValidatorContext constraintValidatorContext)
+        {
+            if (value == null)
+                return true;
+            try
+            {
+                if (((DateTime)value).Kind != DateTimeKind.Utc)
+                    return false;
+            }
+            catch (InvalidCastException)
+            {
+                throw new InvalidOperationException(
+                    "The UtcDateTimeValidator may only be used against DateTime or DateTime? properties.");
+            }
+            return true;
+        }
+    }
+}

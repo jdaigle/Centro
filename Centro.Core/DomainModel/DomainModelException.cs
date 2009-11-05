c970d7ef485067294fc0ce1304fd2567fb151af4
Joseph Daigle <joseph@cridion.com>
2009-11-05 11:25:19 -0500

    forgot a file, also changed some output

---
+++ Centro.Core/DomainModel/DomainModelException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Centro.DomainModel
+{
+    public class DomainModelException : Exception
+    {
+        public DomainModelException()
+        {
+        }
+        public DomainModelException(string message)
+            : base(message)
+        {
+        }
+        public DomainModelException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+    }
+}
